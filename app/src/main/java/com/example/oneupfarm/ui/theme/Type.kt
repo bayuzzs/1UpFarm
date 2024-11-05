ee3f90a088bedfa0cd6108aee51bcd4edb8eab28
Bayu Maulana <bayufransdo@gmail.com>
2024-11-06 02:52:38 +0700

    add poppins font

--- app/src/main/java/com/example/oneupfarm/ui/theme/Type.kt
+++ app/src/main/java/com/example/oneupfarm/ui/theme/Type.kt
@@ -2,18 +2,23 @@ package com.example.oneupfarm.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.oneupfarm.R
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 16.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
