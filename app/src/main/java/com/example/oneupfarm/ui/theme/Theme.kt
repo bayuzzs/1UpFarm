8ddd4dda48a692d2ee7dcafc2e8b7966eee2687a
Bayu Maulana <bayufransdo@gmail.com>
2024-11-22 19:16:38 +0700

    half progress of plant monitoring

--- app/src/main/java/com/example/oneupfarm/ui/theme/Theme.kt
+++ app/src/main/java/com/example/oneupfarm/ui/theme/Theme.kt
@@ -26,8 +26,8 @@ private val LightColorScheme = lightColorScheme(
     surface = SurfaceColor,
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
+    surface = Color(0xFFFFFBFE),
     onSecondary = Color.White,
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
