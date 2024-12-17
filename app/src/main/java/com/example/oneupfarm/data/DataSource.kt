08b1c80a42475506f8a1fc60e430378f645c3b66
Bayu Maulana <77946987+bayuzzs@users.noreply.github.com>
2024-12-18 05:38:26 +0700

    add trophy icon

--- app/src/main/java/com/example/oneupfarm/data/DataSource.kt
+++ app/src/main/java/com/example/oneupfarm/data/DataSource.kt
@@ -16,7 +16,7 @@ object DataSource {
     val navigationItems: List<NavigationItem> = listOf(
         NavigationItem(R.drawable.ic_profile, R.drawable.ic_profile_filled),
         NavigationItem(R.drawable.ic_track_plant, R.drawable.ic_track_plant_filled),
-        NavigationItem(R.drawable.ic_calendar, R.drawable.ic_calendar),
+        NavigationItem(R.drawable.trophy, R.drawable.trophy_filled),
         NavigationItem(R.drawable.ic_marketplace, R.drawable.ic_marketplace),
     )
 
